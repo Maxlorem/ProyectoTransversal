811b028f05590145215be42a533a3e849e20d7d7
AnddyPest <mauripestchanker@gmail.com>
2024-09-27 15:09:39 -0300

    creacion nueva

---
+++ JavaApplication55/src/javaapplication55/JavaApplication55.java
@@ -0,0 +1,17 @@
+
+package javaapplication55;
+
+/**
+ *
+ * @author mauri
+ */
+public class JavaApplication55 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
