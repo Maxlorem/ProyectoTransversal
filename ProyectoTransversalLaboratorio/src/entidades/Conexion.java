1089750f67762c02455cc29b2cc7039a0948b20d
AnddyPest <mauripestchanker@gmail.com>
2024-09-27 16:06:51 -0300

    error de no se q corregido

--- ProyectoTransversalLaboratorio/src/entidades/Conexion.java
+++ ProyectoTransversalLaboratorio/src/entidades/Conexion.java
@@ -1,14 +1,8 @@
+
+
 package entidades;
 
-<<<<<<< HEAD:ProyectoTransversalLaboratorio/src/entidades/coneccion.java
 
-public class coneccion {
-=======
-/**
- *
- * @author mauri
- */
 public class Conexion {
->>>>>>> e64f50235b8372d072bc3b6c87574fedbd9ab711:ProyectoTransversalLaboratorio/src/entidades/Conexion.java
 
 }
