e49ee6e25d192252e86d816584344554ba088d73
AnddyPest <mauripestchanker@gmail.com>
2024-09-30 15:07:56 -0300

    Algo de la vista y algunas imagenes

--- GP11.Universidad/src/vistas/ventanaDeInicio.java
+++ GP11.Universidad/src/vistas/ventanaDeInicio.java
@@ -1,9 +1,12 @@
 package vistas;
 
+import java.awt.Color;
+
 public class ventanaDeInicio extends javax.swing.JFrame {
 
         public ventanaDeInicio() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
@@ -14,18 +17,46 @@ public class ventanaDeInicio extends javax.swing.JFrame {
         menuPnl = new javax.swing.JPanel();
         closePnl = new javax.swing.JPanel();
         closeBtn = new javax.swing.JLabel();
+        btnSistemaInscripciones = new javax.swing.JPanel();
+        lblSysInsc = new javax.swing.JLabel();
+        lblInsc = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        btnSistemaAlumnos = new javax.swing.JPanel();
+        lblSysAlu = new javax.swing.JLabel();
+        lblAlu = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jPanel1 = new javax.swing.JPanel();
+        btnSistemaMaterias = new javax.swing.JPanel();
+        lblSysMat = new javax.swing.JLabel();
+        lblMat = new javax.swing.JLabel();
+        jSeparator3 = new javax.swing.JSeparator();
         imagenPnl = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(1000, 550));
+        setUndecorated(true);
         setResizable(false);
 
         backgroundPnl.setBackground(new java.awt.Color(153, 153, 153));
+        backgroundPnl.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
         backgroundPnl.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        menuPnl.setBackground(new java.awt.Color(51, 51, 51));
         menuPnl.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        closePnl.setBackground(new java.awt.Color(255, 102, 102));
+        closePnl.setBackground(new java.awt.Color(255, 102, 0));
+        closePnl.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        closePnl.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                closePnlMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                closePnlMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                closePnlMouseExited(evt);
+            }
+        });
 
         closeBtn.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         closeBtn.setForeground(new java.awt.Color(255, 255, 255));
@@ -49,6 +80,76 @@ public class ventanaDeInicio extends javax.swing.JFrame {
         );
 
         menuPnl.add(closePnl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 30, 30));
+
+        btnSistemaInscripciones.setBackground(new java.awt.Color(51, 51, 51));
+        btnSistemaInscripciones.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblSysInsc.setFont(new java.awt.Font("Verdana", 1, 18)); // NOI18N
+        lblSysInsc.setForeground(new java.awt.Color(255, 102, 0));
+        lblSysInsc.setText("Sistema");
+        btnSistemaInscripciones.add(lblSysInsc, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 228, -1));
+
+        lblInsc.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        lblInsc.setForeground(new java.awt.Color(255, 255, 255));
+        lblInsc.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lblInsc.setText("INSCRIPCIONES");
+        btnSistemaInscripciones.add(lblInsc, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 180, 31));
+
+        jSeparator1.setForeground(new java.awt.Color(255, 102, 0));
+        btnSistemaInscripciones.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 130, 10));
+
+        menuPnl.add(btnSistemaInscripciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 190, 240, 50));
+
+        btnSistemaAlumnos.setBackground(new java.awt.Color(51, 51, 51));
+        btnSistemaAlumnos.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblSysAlu.setFont(new java.awt.Font("Verdana", 1, 18)); // NOI18N
+        lblSysAlu.setForeground(new java.awt.Color(255, 102, 0));
+        lblSysAlu.setText("Sistema");
+        btnSistemaAlumnos.add(lblSysAlu, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 228, -1));
+
+        lblAlu.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        lblAlu.setForeground(new java.awt.Color(255, 255, 255));
+        lblAlu.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lblAlu.setText("ALUMNOS");
+        btnSistemaAlumnos.add(lblAlu, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 130, 31));
+
+        jSeparator2.setForeground(new java.awt.Color(255, 102, 0));
+        btnSistemaAlumnos.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 130, 10));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 970, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 30, Short.MAX_VALUE)
+        );
+
+        btnSistemaAlumnos.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, -50, 970, 30));
+
+        menuPnl.add(btnSistemaAlumnos, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 240, 50));
+
+        btnSistemaMaterias.setBackground(new java.awt.Color(51, 51, 51));
+        btnSistemaMaterias.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblSysMat.setFont(new java.awt.Font("Verdana", 1, 18)); // NOI18N
+        lblSysMat.setForeground(new java.awt.Color(255, 102, 0));
+        lblSysMat.setText("Sistema");
+        btnSistemaMaterias.add(lblSysMat, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 6, 228, -1));
+
+        lblMat.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        lblMat.setForeground(new java.awt.Color(255, 255, 255));
+        lblMat.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lblMat.setText("MATERIAS");
+        btnSistemaMaterias.add(lblMat, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, 130, 31));
+
+        jSeparator3.setForeground(new java.awt.Color(255, 102, 0));
+        btnSistemaMaterias.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 130, 10));
+
+        menuPnl.add(btnSistemaMaterias, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 240, 50));
 
         backgroundPnl.add(menuPnl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 260, 550));
 
@@ -81,6 +182,19 @@ public class ventanaDeInicio extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void closePnlMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closePnlMouseEntered
+        closePnl.setBackground(Color.red);
+    }//GEN-LAST:event_closePnlMouseEntered
+
+    private void closePnlMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closePnlMouseClicked
+        closePnl.setBackground(Color.YELLOW);
+        System.exit(0);
+    }//GEN-LAST:event_closePnlMouseClicked
+
+    private void closePnlMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closePnlMouseExited
+        closePnl.setBackground(new Color(255, 155, 0));
+    }//GEN-LAST:event_closePnlMouseExited
+
     public static void main(String args[]) {
         
         
@@ -93,9 +207,22 @@ public class ventanaDeInicio extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel backgroundPnl;
+    private javax.swing.JPanel btnSistemaAlumnos;
+    private javax.swing.JPanel btnSistemaInscripciones;
+    private javax.swing.JPanel btnSistemaMaterias;
     private javax.swing.JLabel closeBtn;
     private javax.swing.JPanel closePnl;
     private javax.swing.JPanel imagenPnl;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel lblAlu;
+    private javax.swing.JLabel lblInsc;
+    private javax.swing.JLabel lblMat;
+    private javax.swing.JLabel lblSysAlu;
+    private javax.swing.JLabel lblSysInsc;
+    private javax.swing.JLabel lblSysMat;
     private javax.swing.JPanel menuPnl;
     // End of variables declaration//GEN-END:variables
 }
