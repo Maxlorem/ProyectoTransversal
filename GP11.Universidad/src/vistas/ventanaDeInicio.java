815627965dd9ae0898c401e643f4c44d95920783
AnddyPest <mauripestchanker@gmail.com>
2024-09-30 15:26:47 -0300

    asd

--- GP11.Universidad/src/vistas/ventanaDeInicio.java
+++ GP11.Universidad/src/vistas/ventanaDeInicio.java
@@ -15,8 +15,6 @@ public class ventanaDeInicio extends javax.swing.JFrame {
 
         backgroundPnl = new javax.swing.JPanel();
         menuPnl = new javax.swing.JPanel();
-        closePnl = new javax.swing.JPanel();
-        closeBtn = new javax.swing.JLabel();
         btnSistemaInscripciones = new javax.swing.JPanel();
         lblSysInsc = new javax.swing.JLabel();
         lblInsc = new javax.swing.JLabel();
@@ -31,9 +29,10 @@ public class ventanaDeInicio extends javax.swing.JFrame {
         lblMat = new javax.swing.JLabel();
         jSeparator3 = new javax.swing.JSeparator();
         imagenPnl = new javax.swing.JPanel();
+        closePnl = new javax.swing.JPanel();
+        closeBtn = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(1000, 550));
         setUndecorated(true);
         setResizable(false);
 
@@ -43,43 +42,6 @@ public class ventanaDeInicio extends javax.swing.JFrame {
 
         menuPnl.setBackground(new java.awt.Color(51, 51, 51));
         menuPnl.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        closePnl.setBackground(new java.awt.Color(255, 102, 0));
-        closePnl.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        closePnl.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                closePnlMouseClicked(evt);
-            }
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                closePnlMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                closePnlMouseExited(evt);
-            }
-        });
-
-        closeBtn.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        closeBtn.setForeground(new java.awt.Color(255, 255, 255));
-        closeBtn.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        closeBtn.setText("x");
-        closeBtn.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-
-        javax.swing.GroupLayout closePnlLayout = new javax.swing.GroupLayout(closePnl);
-        closePnl.setLayout(closePnlLayout);
-        closePnlLayout.setHorizontalGroup(
-            closePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, closePnlLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(closeBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        closePnlLayout.setVerticalGroup(
-            closePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, closePnlLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(closeBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-
-        menuPnl.add(closePnl, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 30, 30));
 
         btnSistemaInscripciones.setBackground(new java.awt.Color(51, 51, 51));
         btnSistemaInscripciones.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -155,15 +117,54 @@ public class ventanaDeInicio extends javax.swing.JFrame {
 
         imagenPnl.setBackground(new java.awt.Color(204, 204, 204));
 
+        closePnl.setBackground(new java.awt.Color(255, 102, 0));
+        closePnl.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        closePnl.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                closePnlMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                closePnlMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                closePnlMouseExited(evt);
+            }
+        });
+
+        closeBtn.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        closeBtn.setForeground(new java.awt.Color(255, 255, 255));
+        closeBtn.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        closeBtn.setText("x");
+        closeBtn.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+
+        javax.swing.GroupLayout closePnlLayout = new javax.swing.GroupLayout(closePnl);
+        closePnl.setLayout(closePnlLayout);
+        closePnlLayout.setHorizontalGroup(
+            closePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, closePnlLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(closeBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        closePnlLayout.setVerticalGroup(
+            closePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, closePnlLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(closeBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
         javax.swing.GroupLayout imagenPnlLayout = new javax.swing.GroupLayout(imagenPnl);
         imagenPnl.setLayout(imagenPnlLayout);
         imagenPnlLayout.setHorizontalGroup(
             imagenPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 190, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, imagenPnlLayout.createSequentialGroup()
+                .addGap(0, 159, Short.MAX_VALUE)
+                .addComponent(closePnl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         imagenPnlLayout.setVerticalGroup(
             imagenPnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 550, Short.MAX_VALUE)
+            .addGroup(imagenPnlLayout.createSequentialGroup()
+                .addComponent(closePnl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 520, Short.MAX_VALUE))
         );
 
         backgroundPnl.add(imagenPnl, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 0, 190, 550));
