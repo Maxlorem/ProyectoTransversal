e8f14e87b72ea77faa5ffc9291e4ca19ef8b9bf8
AnddyPest <mauripestchanker@gmail.com>
2024-10-09 12:39:25 -0300

    2 vueltitas mas de rosca

--- GP11.Universidad/src/vistas/VistaMaterias.java
+++ GP11.Universidad/src/vistas/VistaMaterias.java
@@ -179,6 +179,7 @@ public class VistaMaterias extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        tabMaterias.setEnabled(false);
         tabMaterias.setFocusable(false);
         jScrollPane1.setViewportView(tabMaterias);
 
@@ -772,6 +773,7 @@ public class VistaMaterias extends javax.swing.JFrame {
         txtPanel.setText("Seleccione la materia a editar en la tabla, luego seleccione el metodo de edicion que desea utilizar.");
         btnClear.setEnabled(false);
         btnNw.setEnabled(false);
+        tabMaterias.setEnabled(true);
         
 
 
@@ -837,7 +839,7 @@ public class VistaMaterias extends javax.swing.JFrame {
         txtYear.setEditable(false);
         modelo.setRowCount(0);
         llenarTabla();
-        tabMaterias.setEnabled(true);
+        tabMaterias.setEnabled(false);
         txtPanel.setText("");
         
 
