94edb844b7b22f181501bf2e9b87a4383d42541e
AnddyPest <mauripestchanker@gmail.com>
2024-10-09 20:26:22 -0300

    mini push

--- GP11.Universidad/src/vistas/VistaAlumnos.java
+++ GP11.Universidad/src/vistas/VistaAlumnos.java
@@ -688,24 +688,32 @@ public class VistaAlumnos extends javax.swing.JFrame {
         jRporId.setEnabled(true);
         jRporApellido.setEnabled(true);
         grpSelection.clearSelection();
+        txtBusqueda.setText("");
+        txtBusqueda.setEditable(false);
+        btnSrc.setEnabled(false);
+        btnAccept.setEnabled(false);
     }//GEN-LAST:event_btnSrcActionPerformed
 
     private void btnAcceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAcceptActionPerformed
+        txtBusqueda.setEnabled(true);
         txtBusqueda.requestFocus();
         if (jRporId.isSelected()) {
             jRporApellido.setEnabled(false);
             jRporDni.setEnabled(false);
+            jRporId.setEnabled(false);
             txtBusqueda.setEditable(true);
             txtMsg.setText("Realizará una búsqueda por ID, asegúrese de ingresar solo NUMEROS en el campo de búsqueda.");
         } else if (jRporApellido.isSelected()) {
             jRporId.setEnabled(false);
             jRporDni.setEnabled(false);
+            jRporApellido.setEnabled(false);
             txtBusqueda.setEditable(true);
             txtMsg.setText("Realizará una búsqueda por Apellido, asegúrese de conocer el apellido de los alumnos que desea buscar con este método de búsqueda.");
 
         } else if (jRporDni.isSelected()) {
             jRporId.setEnabled(false);
             jRporApellido.setEnabled(false);
+            jRporDni.setEnabled(false);
             txtBusqueda.setEditable(true);
             txtMsg.setText("Realizará una búsqueda por DNI, asegúrese de ingresar solo NUMEROS en el campo de busqueda.");
         }
@@ -727,6 +735,7 @@ public class VistaAlumnos extends javax.swing.JFrame {
         btnAccept.setEnabled(false);
         btnSrc.setEnabled(false);
         txtBusqueda.setText("");
+        txtBusqueda.setEnabled(false);
         modelo.setRowCount(0);
         llenarTabla();
         txtMsg.setText("");
